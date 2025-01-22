d384a63b14537afe13d9e771813dff7d0cca5d09
R3pic <101osc@naver.com>
2025-01-22 14:04:15 +0900

    feat: 헤더 아이콘 추가

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link';
 import style from './Header.module.css';
 import { Package } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Header() {
+    const username = '유저이름';
+
     return (
         <header className={style.header}>
             <Link href={'/'} className={style.brand}>
                 <Package className={style.icon} size='2rem'/>
-                <span className={style['site-name']}>꾸러미탑</span>
+                <span className={style.siteName}>꾸러미탑</span>
             </Link>
+
+            <div className={style.toolBar}>
+                <div className={style.profile}>
+                    <Image className={style.profileIcon} src={'/profile.webp'} width={50} height={50} alt={'profile icon'}/>
+                    <span className={style.userName}>{ username }</span>
+                </div>
+            </div>
         </header>
     );
 }
