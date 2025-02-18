86c524e313496f8cc894d3e17e6640dc06476822
R3pic <101osc@naver.com>
2025-02-19 01:41:59 +0900

    feat: 메모 삭제 기능 구현

--- src/components/comment/comment-card.tsx
+++ src/components/comment/comment-card.tsx
@@ -1,14 +1,55 @@
 import {Comment} from '@/types';
 import {formatDateString} from '@/lib/string-utils.ts';
 import {Card} from '@/components/ui/card.tsx';
+import {
+  AlertDialog, AlertDialogAction, AlertDialogCancel,
+  AlertDialogContent, AlertDialogDescription, AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog.tsx';
+import {Button} from '@/components/ui/button.tsx';
+import {X} from 'lucide-react';
+import axiosInstance from '@/api/api.ts';
+import {useNavigate} from 'react-router';
 
 interface Props {
   comment: Comment;
 }
 
 export default function CommentCard({ comment }: Props) {
+  const navigate = useNavigate();
+  async function deleteCommentClick() {
+    console.log('삭제 확인버튼 누름');
+
+    const response = await axiosInstance.delete(`/comments/${comment.id}`);
+
+    if (response.status === 204) {
+      navigate(0);
+    }
+  }
+
   return (
-    <Card className='w-1/2 p-2'>
+    <Card className='relative w-1/2 p-2'>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button className='absolute right-1 top-1' variant='ghost' size='xs' >
+            <X />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>정말로 삭제하시겠습니까?</AlertDialogTitle>
+            <AlertDialogDescription>
+              메모를 삭제하면 다시 되돌릴 수 없습니다. 괜찮다면 확인을 눌러주세요.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>취소</AlertDialogCancel>
+            <AlertDialogAction onClick={deleteCommentClick}>확인</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
       <p className='mb-2 leading-5 line-clamp-2 '>{ comment.content }</p>
       <p className='text-right text-xs text-muted-foreground'>{ formatDateString(comment.created_at) }</p>
     </Card>
